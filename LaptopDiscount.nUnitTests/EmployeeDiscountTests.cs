264b5185849ddcf26b16dedb0e9272ca92be06f4
varun <varunjohn@gmail.com>
2024-06-17 12:33:09 -0400

    6 test cases added

--- LaptopDiscount.nUnitTests/EmployeeDiscountTests.cs
+++ LaptopDiscount.nUnitTests/EmployeeDiscountTests.cs
@@ -12,5 +12,97 @@ namespace LaptopDiscount.nUnitTests
         {
             _employeeDiscount = new EmployeeDiscount();
         }
+
+        [Test]
+        public void Price_NoDiscountShouldBeProvided()
+        {
+            // Arrange
+            _employeeDiscount.Price = 100;
+            decimal expectedPriceAfterDiscount = 100;
+
+            // Act
+            decimal calculatedPriceWithDiscount = _employeeDiscount.CalculateDiscountedPrice();
+
+            // Assert
+            Assert.That(calculatedPriceWithDiscount, Is.EqualTo(expectedPriceAfterDiscount));
+        }
+
+        [Test]
+        public void NullEmployeeType_NoDiscountShouldBeProvided()
+        {
+            // Arrange
+            _employeeDiscount.Price = 100;
+            decimal expectedPriceAfterDiscount = 100;
+
+            // Act
+            decimal calculatedPriceWithDiscount = _employeeDiscount.CalculateDiscountedPrice();
+
+            // Assert
+            Assert.That(calculatedPriceWithDiscount, Is.EqualTo(expectedPriceAfterDiscount));
+        }
+
+        [Test]
+        public void PartTime_NoDiscountShouldBeProvided()
+        {
+            // Arrange 
+            // 0 discount should be provided
+            _employeeDiscount.Price = 100;
+            _employeeDiscount.EmployeeType = EmployeeType.PartTime;
+            decimal expectedPriceAfterDiscount = 100;
+
+            // Act
+            decimal calculatedPriceWithDiscount = _employeeDiscount.CalculateDiscountedPrice();
+
+            // Assert
+            Assert.That(calculatedPriceWithDiscount, Is.EqualTo(expectedPriceAfterDiscount));
+        }
+
+        [Test]
+        public void PartialLoad_DiscountShouldBeProvided()
+        {
+            // Arrange
+            // 5% discount should be provided
+            _employeeDiscount.Price = 100;
+            _employeeDiscount.EmployeeType = EmployeeType.PartialLoad;
+            decimal expectedPriceAfterDiscount = 95;
+
+            // Act
+            decimal calculatedPriceWithDiscount = _employeeDiscount.CalculateDiscountedPrice();
+
+            // Assert
+            Assert.That(calculatedPriceWithDiscount, Is.EqualTo(expectedPriceAfterDiscount));
+        }
+
+        [Test]
+        public void FullTime_DiscountShouldBeProvided()
+        {
+            // Arrange
+            // 10% discount should be provided
+            _employeeDiscount.Price = 100;
+            _employeeDiscount.EmployeeType = EmployeeType.FullTime;
+            decimal expectedPriceAfterDiscount = 90;
+
+            // Act
+            decimal calculatedPriceWithDiscount = _employeeDiscount.CalculateDiscountedPrice();
+
+            // Assert
+            Assert.That(calculatedPriceWithDiscount, Is.EqualTo(expectedPriceAfterDiscount));
+        }
+
+        [Test]
+        public void Data_ShouldBeSetCorrectly()
+        {
+            // Arrange
+            // 20% discount should be provided
+            _employeeDiscount.Price = 100;
+            _employeeDiscount.EmployeeType = EmployeeType.FullTime;
+            decimal expectedPrice = 100;
+            EmployeeType expectedEmployeeType = EmployeeType.FullTime;
+
+
+            // Act & Assert
+            Assert.That(_employeeDiscount.Price, Is.EqualTo(expectedPrice));
+            Assert.That(_employeeDiscount.EmployeeType, Is.EqualTo(expectedEmployeeType));
+        }
     }
 }
